23b2cdda8b8b0381462be3d61de885317cd60e83
Joao Fernandes <joaopfernandesc@gmail.com>
2021-04-03 21:17:20 -0300

    Adds Joi validation

--- src/modules/connectors/infra/http/routes/connectors.routes.ts
+++ src/modules/connectors/infra/http/routes/connectors.routes.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ConnectorsController from '../controllers/ConnectorsController';
 
 const connectorsRouter = Router();
 const connectorsController = new ConnectorsController();
+connectorsRouter.use(ensureAuthenticated);
 
-connectorsRouter.post('/', connectorsController.create);
+connectorsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      type: Joi.string().valid('REST', 'SOAP', 'BD').required(),
+      privacy: Joi.string().valid('PUBLIC', 'PRIVATE').required(),
+      baseUrl: Joi.string().uri().required(),
+      logoUrl: Joi.string().uri().required(),
+      category: Joi.string().required(),
+      description: Joi.string().required(),
+      status: Joi.string().required(),
+    },
+  }),
+  connectorsController.create,
+);
 
 export default connectorsRouter;
